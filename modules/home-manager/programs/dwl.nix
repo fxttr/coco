2e2375aeb6c1fc2e63ec5a7e4108e6514bf9817f
Florian Büstgens <fb@fx-ttr.de>
2023-05-01 07:48:14 +0200

    Adding dwl

---
+++ modules/home-manager/programs/dwl.nix
@@ -0,0 +1,17 @@
+{ self, config, lib, pkgs, inputs, ... }:
+
+with lib;
+
+let
+  cfg = config.coco.dwl;
+in
+{
+  options.coco.dwl.enable = mkEnableOption "Install dwl";
+
+  config = mkIf cfg.enable
+    {
+      wayland.windowManager.dwl = {
+        enable = true;
+      };
+    }
+}
