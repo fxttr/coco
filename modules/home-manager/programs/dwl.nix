db8009d0b46d85979684e54378e7bda84d350b31
Florian Büstgens <fb@fx-ttr.de>
2023-05-01 08:07:55 +0200

    add dwl package

--- modules/home-manager/programs/dwl.nix
+++ modules/home-manager/programs/dwl.nix
@@ -10,8 +10,21 @@ in
 
   config = mkIf cfg.enable
     {
-      wayland.windowManager.dwl = {
-        enable = true;
+      home.packages = (pkgs.dwl.overrideAttrs (old: {
+            src = pkgs.fetchFromGitHub {
+              owner = "fxttr";
+              repo = "dwl";
+              rev = "eb2234c0312836824438313dbcde8bd5888ee2da";
+              sha256 = "sha256-iK6r+3XwTMPItSuLK+sex0DoDWJ9ApAfhivFGM5CF1Y=";
+            };
+          }));
+      systemd.user.targets.dwl-session = {
+        Unit = {
+          Description = "dwl compositor session";
+          Documentation = ["man:systemd.special(7)"];
+          BindsTo = ["wayland-session.target"];
+          After = ["wayland-session.target"];
+        };
       };
     };
 }
