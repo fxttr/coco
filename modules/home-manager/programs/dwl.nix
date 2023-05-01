7fd4c6801b2d4ae1a9ab2dd0d9e39396f2a9af96
Florian Büstgens <fb@fx-ttr.de>
2023-05-01 08:13:03 +0200

    Add missing packages to dwl.nix

--- modules/home-manager/programs/dwl.nix
+++ modules/home-manager/programs/dwl.nix
@@ -10,14 +10,19 @@ in
 
   config = mkIf cfg.enable
     {
-      home.packages = (pkgs.dwl.overrideAttrs (old: {
+      home.packages = [
+        (pkgs.dwl.overrideAttrs (old: {
             src = pkgs.fetchFromGitHub {
               owner = "fxttr";
               repo = "dwl";
               rev = "eb2234c0312836824438313dbcde8bd5888ee2da";
               sha256 = "sha256-iK6r+3XwTMPItSuLK+sex0DoDWJ9ApAfhivFGM5CF1Y=";
             };
-          }));
+          }))
+          alacritty
+          waylock
+          bemenu
+          ];
       systemd.user.targets.dwl-session = {
         Unit = {
           Description = "dwl compositor session";
