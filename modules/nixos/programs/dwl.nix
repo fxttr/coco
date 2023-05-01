98a98889640eb892de74138cc8f1eea93341bd63
Florian Büstgens <fb@fx-ttr.de>
2023-05-01 19:20:51 +0200

    Removing defaultSession

--- modules/nixos/programs/dwl.nix
+++ modules/nixos/programs/dwl.nix
@@ -8,16 +8,6 @@ in {
   options.coco.dwl.enable = mkEnableOption "Install dwl";
 
   config = mkIf cfg.enable {
-
-#      extraSessionCommands = ''
-#        			export SDL_VIDEODRIVER=wayland
-#        			export QT_QPA_PLATFORM=wayland
-#              export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-#              export _JAVA_AWT_WM_NONREPARENTING=1
-#              export MOZ_ENABLE_WAYLAND=1
-#        		'';
-#    };
-
     programs.waybar.enable = true;
 
     services.xserver = {
@@ -27,7 +17,6 @@ in {
         lightdm.enable = true;
       };
 
-      desktopManager.defaultSession = "dwl";
       desktopManager.session =
         [ { manage = "desktop";
             name = "dwl";
