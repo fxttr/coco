18af0092b4fb94f1c8d8b5726ea914c6f213a5ac
Florian Büstgens <fb@fx-ttr.de>
2023-06-15 06:34:27 +0200

    Updating mail config

--- modules/home-manager/programs/mail.nix
+++ modules/home-manager/programs/mail.nix
@@ -41,7 +41,7 @@ let
 in {
   options.coco.mail.enable = mkEnableOption "Install and configure tools for mail usage";
   options.coco.mail.accounts = mkOption {
-    type = hm.types.DagOf accountModule;
+    type = hm.types.dagOf accountModule;
     default = {};
     example = literalExpression ''
             "uni" = {
