4c2e9d95369c34b8320aa5e74c6bf0af0f796805
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-11-02 13:10:39 +0100

    Add vulkan renderer to sway

--- modules/nixos/programs/sway.nix
+++ modules/nixos/programs/sway.nix
@@ -21,9 +21,10 @@ in {
         ranger
       ];
       extraSessionCommands = ''
-        			export SDL_VIDEODRIVER=wayland
+      	      export SDL_VIDEODRIVER=wayland
               export CLUTTER_BACKEND=wayland
-        			export QT_QPA_PLATFORM=wayland
+              export QT_QPA_PLATFORM=wayland
+	      export WLR_RENDERER=vulkan
               export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
               export _JAVA_AWT_WM_NONREPARENTING=1
               export MOZ_ENABLE_WAYLAND=1
