e8123670bc6f44aac0050ad0b529eb5ec6cfc82a
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-11-04 09:17:00 +0100

    Format sway.nix and update flake

--- modules/nixos/programs/sway.nix
+++ modules/nixos/programs/sway.nix
@@ -21,20 +21,20 @@ in {
         ranger
       ];
       extraOptions = [
-	"--unsupported-gpu"
+        "--unsupported-gpu"
       ];
       extraSessionCommands = ''
-      	      export SDL_VIDEODRIVER=wayland
-              export CLUTTER_BACKEND=wayland
-              export QT_QPA_PLATFORM=wayland
-	      export WLR_RENDERER=vulkan
-              export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-              export _JAVA_AWT_WM_NONREPARENTING=1
-              export MOZ_ENABLE_WAYLAND=1
-              export _JAVA_AWT_WM_NONREPARENTING=1
-              export WLR_NO_HARDWARE_CURSORS=1
-              export XWAYLAND_NO_GLAMOR=1
-        		'';
+              	      export SDL_VIDEODRIVER=wayland
+                      export CLUTTER_BACKEND=wayland
+                      export QT_QPA_PLATFORM=wayland
+        	            export WLR_RENDERER=vulkan
+                      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+                      export _JAVA_AWT_WM_NONREPARENTING=1
+                      export MOZ_ENABLE_WAYLAND=1
+                      export _JAVA_AWT_WM_NONREPARENTING=1
+                      export WLR_NO_HARDWARE_CURSORS=1
+                      export XWAYLAND_NO_GLAMOR=1
+                		'';
     };
 
     programs.waybar.enable = true;
