ad9fc1a78881aec842f80214abf047ed4597fbf5
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-11-23 10:57:59 +0100

    Add theme

---
+++ modules/home-manager/theme.nix
@@ -0,0 +1,76 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let cfg = config.coco.theme;
+
+in {
+  options.coco.gnome.enable = mkEnableOption "Install and configure gnome";
+
+  config = mkIf cfg.enable {
+    programs.dconf = {
+      settings = {
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          enable-hot-corners = false;
+        };
+        "org/gnome/shell" = {
+          favorite-apps = [
+            "firefox.desktop"
+            "org.gnome.Nautilus.desktop"
+            "code.desktop"
+            "obsidian.desktop"
+            "org.gnome.Terminal.desktop"
+            "spotify.desktop"
+          ];
+
+          disable-user-extensions = false;
+          enabled-extensions = [
+            "user-theme@gnome-shell-extensions.gcampax.github.com"
+            "dash-to-panel@jderose9.github.com"
+          ];
+
+          "org/gnome/shell/extensions/user-theme" = {
+            name = "palenight";
+          };
+        };
+      };
+
+      home.packages = with pkgs; [
+        gnomeExtensions.user-themes
+        gnomeExtensions.dash-to-panel
+        palenight-theme
+      ];
+
+      gtk = {
+        enable = true;
+
+        iconTheme = {
+          name = "Papirus-Dark";
+          package = pkgs.papirus-icon-theme;
+        };
+
+        theme = {
+          name = "palenight";
+          package = pkgs.palenight-theme;
+        };
+
+        cursorTheme = {
+          name = "Numix-Cursor";
+          package = pkgs.numix-cursor-theme;
+        };
+
+        gtk3.extraConfig = {
+          Settings = ''
+            gtk-application-prefer-dark-theme=1
+          '';
+        };
+
+        gtk4.extraConfig = {
+          Settings = ''
+            gtk-application-prefer-dark-theme=1
+          '';
+        };
+      };
+    };
+  }
--- modules/nixos/programs/gnome.nix
+++ modules/nixos/programs/gnome.nix
@@ -16,27 +16,30 @@ in {
       xkbVariant = "altgr-intl";
 
       displayManager.gdm.enable = true;
-      desktopManager.gnome= {
+      desktopManager.gnome = {
         enable = true;
       };
     };
 
     environment.gnome.excludePackages = (with pkgs; [
-          gnome-photos
-          gnome-tour
-        ]) ++ (with pkgs.gnome; [
-          cheese # webcam tool
-          gnome-music
-          gnome-terminal
-          gedit # text editor
-          epiphany # web browser
-          geary # email reader
-          evince # document viewer
-          gnome-characters
-          totem # video player
-          tali # poker game
-          iagno # go game
-          hitori # sudoku game
-        ]);
-  };
-}
+      gnome-photos
+      gnome-tour
+    ]) ++ (with pkgs.gnome; [
+      cheese # webcam tool
+      gnome-music
+      gnome-terminal
+      gedit # text editor
+      epiphany # web browser
+      geary # email reader
+      evince # document viewer
+      gnome-characters
+      totem # video player
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+    ]);
+    environment.systemPackages = with pkgs; [
+      gnome.gnome-tweaks
+    ]
+      };
+  }
