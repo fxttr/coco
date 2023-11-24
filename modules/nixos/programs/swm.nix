ccec4fc58ceb07c0f1a7bd855cb6572743469585
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-11-24 22:18:46 +0100

    Update stc

--- modules/nixos/programs/swm.nix
+++ modules/nixos/programs/swm.nix
@@ -2,8 +2,14 @@
 
 with lib;
 
-let cfg = config.coco.swm;
-
+let
+  cfg = config.coco.swm;
+  swm = (pkgs.fetchFromGitHub {
+    owner = "fxttr";
+    repo = "swm";
+    rev = "2578e270c7f982364e2f553393041c58593647f4";
+    sha256 = "sha256-kmnssxJ4L+OgbroeSWLRSErpGEDkQfEdZBUDOq3DTaI=";
+  });
 in
 {
   options.coco.swm.enable = mkEnableOption "Install swm";
@@ -23,12 +29,7 @@ in
           manage = "desktop";
           name = "Swm";
           start = ''
-            ${(pkgs.fetchFromGitHub {
-              owner = "fxttr";
-              repo = "swm";
-              rev = "2578e270c7f982364e2f553393041c58593647f4";
-              sha256 = "sha256-kmnssxJ4L+OgbroeSWLRSErpGEDkQfEdZBUDOq3DTaI=";
-            }).defaultPackage.x86_64-linux}/bin/swm
+            ${swm.defaultPackage.x86_64-linux}/bin/swm
           '';
         }];
 
