6326249329e904d30b75699dcb93368a456e9206
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-11-24 14:27:15 +0100

    Add swm and stc overlays

--- modules/home-manager/programs/swm.nix
+++ modules/home-manager/programs/swm.nix
@@ -4,6 +4,23 @@ with lib;
 
 let
   cfg = config.coco.swm;
+  pkgs.overlays = with builtins; [
+    import
+    (fetchFromGitHub {
+      owner = "fxttr";
+      repo = "swm";
+      rev = "1711b7699aa1ae633fe415d86d6c8560bc2d6cf4";
+      sha256 = "sha256-kmnssxJ4L+OgbroeSWLRSErpGEDkQfEdZBUDOq3DTaI=";
+    })
+    import
+    (fetchFromGitHub {
+      owner = "fxttr";
+      repo = "stc";
+      rev = "a128cc725efc5ef4cec7a4c72b156584f80857af";
+      sha256 = "sha256-Ly8FNYp0lezd7rqkbldmnbImexrdla5ZfmIVXyOwwEU=";
+    })
+  ];
+
 in
 {
   options.coco.swm.enable = mkEnableOption "Install swm";
@@ -11,18 +28,8 @@ in
   config = mkIf cfg.enable
     {
       home.packages = [
-        (pkgs.fetchFromGitHub {
-          owner = "fxttr";
-          repo = "swm";
-          rev = "1711b7699aa1ae633fe415d86d6c8560bc2d6cf4";
-          sha256 = "sha256-kmnssxJ4L+OgbroeSWLRSErpGEDkQfEdZBUDOq3DTaI=";
-        })
-        (pkgs.fetchFromGitHub {
-          owner = "fxttr";
-          repo = "stc";
-          rev = "a128cc725efc5ef4cec7a4c72b156584f80857af";
-          sha256 = "sha256-Ly8FNYp0lezd7rqkbldmnbImexrdla5ZfmIVXyOwwEU=";
-        })
+        pkgs.swm
+        pkgs.stc
         pkgs.feh
         pkgs.dmenu
       ];
