cba2f2298086c5e7e828b2910ea4be17e6922b4d
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-11-24 19:17:11 +0100

    Fix overlays

--- modules/home-manager/programs/swm.nix
+++ modules/home-manager/programs/swm.nix
@@ -5,20 +5,25 @@ with lib;
 let
   cfg = config.coco.swm;
   pkgs.overlays = with builtins; [
-    import
-    (fetchFromGitHub {
-      owner = "fxttr";
-      repo = "swm";
-      rev = "1711b7699aa1ae633fe415d86d6c8560bc2d6cf4";
-      sha256 = "sha256-kmnssxJ4L+OgbroeSWLRSErpGEDkQfEdZBUDOq3DTaI=";
-    })
-    import
-    (fetchFromGitHub {
-      owner = "fxttr";
-      repo = "stc";
-      rev = "a128cc725efc5ef4cec7a4c72b156584f80857af";
-      sha256 = "sha256-Ly8FNYp0lezd7rqkbldmnbImexrdla5ZfmIVXyOwwEU=";
-    })
+    (self: super:
+      {
+        swm = fetchFromGitHub {
+          owner = "fxttr";
+          repo = "swm";
+          rev = "1711b7699aa1ae633fe415d86d6c8560bc2d6cf4";
+          sha256 = "sha256-kmnssxJ4L+OgbroeSWLRSErpGEDkQfEdZBUDOq3DTaI=";
+        };
+      }
+    )
+    (self: super:
+      {
+        stc = fetchFromGitHub {
+          owner = "fxttr";
+          repo = "stc";
+          rev = "a128cc725efc5ef4cec7a4c72b156584f80857af";
+          sha256 = "sha256-Ly8FNYp0lezd7rqkbldmnbImexrdla5ZfmIVXyOwwEU=";
+        };
+      })
   ];
 
 in
