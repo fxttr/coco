77e2185300021da1c66a903beb07f52884f2e377
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-11-24 19:28:14 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -7,15 +7,23 @@
       url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    swm = {
+      url = "github:fxttr/swm";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stc = {
+      url = "github:fxttr/stc";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, swm, stc, ... }@inputs:
     let
       lib = inputs;
 
     in
     {
-      inherit lib;
+      inherit lib swm stc;
 
       nixosModule = inputs.self.nixosModules.nixos;
 
--- modules/home-manager/programs/swm.nix
+++ modules/home-manager/programs/swm.nix
@@ -1,31 +1,9 @@
-{ self, config, lib, pkgs, inputs, ... }:
+{ self, config, lib, pkgs, inputs, swm, stc, ... }:
 
 with lib;
 
 let
   cfg = config.coco.swm;
-  pkgs.overlays = with builtins; [
-    (self: super:
-      {
-        swm = fetchFromGitHub {
-          owner = "fxttr";
-          repo = "swm";
-          rev = "1711b7699aa1ae633fe415d86d6c8560bc2d6cf4";
-          sha256 = "sha256-kmnssxJ4L+OgbroeSWLRSErpGEDkQfEdZBUDOq3DTaI=";
-        };
-      }
-    )
-    (self: super:
-      {
-        stc = fetchFromGitHub {
-          owner = "fxttr";
-          repo = "stc";
-          rev = "a128cc725efc5ef4cec7a4c72b156584f80857af";
-          sha256 = "sha256-Ly8FNYp0lezd7rqkbldmnbImexrdla5ZfmIVXyOwwEU=";
-        };
-      })
-  ];
-
 in
 {
   options.coco.swm.enable = mkEnableOption "Install swm";
@@ -33,8 +11,8 @@ in
   config = mkIf cfg.enable
     {
       home.packages = [
-        pkgs.swm
-        pkgs.stc
+        swm
+        stc
         pkgs.feh
         pkgs.dmenu
       ];
