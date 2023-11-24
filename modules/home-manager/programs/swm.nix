1f6a0daf7ef1a4e018f506f941ce563cfdde9ed9
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-11-24 19:35:34 +0100

    Update flake

--- modules/home-manager/programs/swm.nix
+++ modules/home-manager/programs/swm.nix
@@ -11,13 +11,13 @@ in
   config = mkIf cfg.enable
     {
       home.packages = [
-        (lib.fetchFromGitHub {
+        (pkgs.fetchFromGitHub {
           owner = "fxttr";
           repo = "swm";
           rev = "1711b7699aa1ae633fe415d86d6c8560bc2d6cf4";
           sha256 = "sha256-kmnssxJ4L+OgbroeSWLRSErpGEDkQfEdZBUDOq3DTaI=";
         })
-        (lib.fetchFromGitHub {
+        (pkgs.fetchFromGitHub {
           owner = "fxttr";
           repo = "stc";
           rev = "a128cc725efc5ef4cec7a4c72b156584f80857af";
